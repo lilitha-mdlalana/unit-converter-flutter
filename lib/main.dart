f4dcd55c1a53b641db0437e28746691c656d9c71
totalgeek-jr <jranonymous517@gmail.com>
2022-01-04 17:55:54 +0200

    Add convert method

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double? _numberFrom;
   String? _startMeasure;
+  String? _convertedMeasure;
   final List<String> _measures = [
     'meters',
     'kilometers',
@@ -40,7 +41,26 @@ class _HomePageState extends State<HomePage> {
     'pounds (lbs)',
     'ounces',
   ];
-
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
   @override
   void initState() {
     _numberFrom = 0;
@@ -63,17 +83,19 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: Container(
-        padding:const EdgeInsets.symmetric(horizontal:20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
-            Spacer(),
-            Text('Value',style: labelStyle,),
-            Spacer(),
+            const Spacer(),
+            Text(
+              'Value',
+              style: labelStyle,
+            ),
+            const Spacer(),
             TextField(
               style: inputStyle,
               decoration: const InputDecoration(
-                hintText:'Please insert the measure to be converted'
-              ),
+                  hintText: 'Please insert the measure to be converted'),
               onChanged: (text) {
                 var rv = double.tryParse(text);
                 if (rv != null) {
@@ -83,27 +105,89 @@ class _HomePageState extends State<HomePage> {
                 }
               },
             ),
-            // Text((_numberFrom == null) ? '' : _numberFrom.toString()),
-            // DropdownButton<String>(
-            //   items: _measures.map((String value) {
-            //     return DropdownMenuItem<String>(
-            //       value: value,
-            //       child: Text(value),
-            //     );
-            //   }).toList(),
-            //   onChanged: (value) {
-            //     setState(
-            //       () {
-            //         _startMeasure = value;
-            //       },
-            //     );
-            //   },
-            //   value: _startMeasure,
-            // )
+            const Spacer(),
+            Text(
+              'From',
+              style: labelStyle,
+            ),
+
+            //Text((_numberFrom == null) ? '' : _numberFrom.toString()),
+            DropdownButton<String>(
+              isExpanded: true,
+              items: _measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: labelStyle,
+                  ),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(
+                  () {
+                    _startMeasure = value;
+                  },
+                );
+              },
+              value: _startMeasure,
+            ),
+            const Spacer(),
+            Text(
+              'To',
+              style: labelStyle,
+            ),
+            const Spacer(),
+            DropdownButton<String>(
+              isExpanded: true,
+              style: inputStyle,
+              items: _measures.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                    value,
+                    style: labelStyle,
+                  ),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _convertedMeasure = value;
+                });
+              },
+              value: _convertedMeasure,
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+            RaisedButton(
+              onPressed: () => true,
+              child: Text(
+                'Convert',
+                style: inputStyle,
+              ),
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+            Text(
+              (_numberFrom == null) ? '' : _numberFrom.toString(),
+              style: labelStyle,
+            ),
+            const Spacer(
+              flex: 8,
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void convert(double value, String from, String to) {
+    int? nFrom = _measuresMap[from];
+    int? nTo = _measuresMap[to];
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
   }
 }
 //80
