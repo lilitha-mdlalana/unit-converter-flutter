7c98be913a6dc3750ccdfdba4cdfcc72f521176b
totalgeek-jr <jranonymous517@gmail.com>
2022-01-04 18:26:28 +0200

    Fix bottom overflow error

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Unit Converter'),
         centerTitle: true,
